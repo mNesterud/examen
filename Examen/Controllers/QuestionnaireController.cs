eed6dae8a35082a96fab83fa4e0b527028743b5a
mNesterud <mnesterud@gmail.com>
2017-04-12 18:18:18 +0200

    Kan nu starta enkät, ladda fråga och svar och byta till nästa fråga.

---
+++ Examen/Controllers/QuestionnaireController.cs
@@ -0,0 +1,30 @@
+﻿using Examen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Examen.Controllers
+{
+    public class QuestionnaireController : Controller
+    {
+        // GET: Questionnaire
+        public ActionResult Questionnaire()
+        {
+            QuestionnaireModel q = new QuestionnaireModel(true);
+            return View(q);
+        }
+        public PartialViewResult Question(string id)
+        {
+            QuestionAndResponses qr = new QuestionAndResponses(Convert.ToInt16(id));
+            //Question q = new Question();
+
+            //using (DBEntities db = new DBEntities()){
+            //    q = db.Questions.Where(x => x.Id.ToString() == id).FirstOrDefault();
+            //}
+
+            return PartialView("Question", qr);
+        }
+    }
+}
---
+++ Examen/Models/QuestionAndResponses.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Examen.Models
+{
+    public class QuestionAndResponses
+    {
+        public Question Q { get; set; }
+        public List<Response> Responses { get; set; }
+        public int SelectedResponseId { get; set; }
+
+        public QuestionAndResponses(int QuestionId)
+        {
+            using (DBEntities db = new DBEntities())
+            {
+                Q = db.Questions.Where(x => x.Id == QuestionId).FirstOrDefault();
+                Responses = new List<Response>();
+                foreach (QuestionResponse qr in db.QuestionResponses)
+                {
+                    if (qr.QuestionID == QuestionId)
+                    {
+                        Responses.Add(db.Responses.Where(x => x.Id == qr.ResponseID).FirstOrDefault());
+                    }
+                }
+            }
+            SelectedResponseId = 0;
+
+        }
+    }
+}
