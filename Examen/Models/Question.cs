8d46abc8ad327389608f5500920809ce75771e3e
ansv <ansv1410@student.miun.se>
2017-04-23 21:13:52 +0200

    Uppdaterat Question-tabellen med en HelpText-kolumn. Lite mer CSS. Ny bullet-text

--- Examen/Models/Question.cs
+++ Examen/Models/Question.cs
@@ -24,6 +24,7 @@ namespace Examen.Models
         public int Id { get; set; }
         public string QuestionText { get; set; }
         public bool MultipleChoise { get; set; }
+        public string HelpText { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<QuestionResponse> QuestionResponses { get; set; }
