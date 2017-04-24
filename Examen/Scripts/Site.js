adc98af12a92d252db8179e234a3bf84f7856b5a
ansv <ansv1410@student.miun.se>
2017-04-24 15:15:41 +0200

    Dokumentformattering och kommentarer BORTA

--- Examen/Scripts/Site.js
+++ Examen/Scripts/Site.js
@@ -24,3 +24,33 @@ function preventUnload() {
         return "uppdatering är låst";
     });
 }
+
+function getBrowserType() {
+    var agent = navigator.userAgent;
+    var userBrowser = "";
+
+    if (agent.indexOf("Edge") > -1) {
+        userBrowser = "Edge";
+    }
+    else if (agent.indexOf("Chrome") > -1) {
+        userBrowser = "Chrome";
+    }
+    else if (agent.indexOf("Safari") > -1) {
+        userBrowser = "Safari";
+    }
+    else if (agent.indexOf("Opera") > -1) {
+        userBrowser = "Opera";
+    }
+    else if (agent.indexOf("Firefox") > -1) {
+        userBrowser = "Firefox";
+    }
+    else if (agent.indexOf("MSIE") > -1) {
+        userBrowser = "Internet Explorer";
+    }
+    else if (agent.indexOf("Trident") > -1) {
+        userBrowser = "Internet Explorer";
+    }
+
+    userBrowser += " - " + agent
+    return userBrowser;
+}
