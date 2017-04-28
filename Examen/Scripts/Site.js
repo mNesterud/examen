64190e25e8d750b286ed076852930f4c048c29e8
mNesterud <mnesterud@gmail.com>
2017-04-28 18:00:01 +0200

    uppdatering på js qrBrowserType

--- Examen/Scripts/Site.js
+++ Examen/Scripts/Site.js
@@ -41,31 +41,40 @@ function preKillSession() {
 //}
 
 function getBrowserType() {
-    var agent = navigator.userAgent;
+
     var userBrowser = "";
+    var agent = "Other";
 
-    if (agent.indexOf("Edge") > -1) {
-        userBrowser = "Edge";
-    }
-    else if (agent.indexOf("Chrome") > -1) {
-        userBrowser = "Chrome";
-    }
-    else if (agent.indexOf("Safari") > -1) {
-        userBrowser = "Safari";
-    }
-    else if (agent.indexOf("Opera") > -1) {
-        userBrowser = "Opera";
-    }
-    else if (agent.indexOf("Firefox") > -1) {
-        userBrowser = "Firefox";
-    }
-    else if (agent.indexOf("MSIE") > -1) {
-        userBrowser = "Internet Explorer";
-    }
-    else if (agent.indexOf("Trident") > -1) {
-        userBrowser = "Internet Explorer";
-    }
+    if (navigator.userAgent != null)
+    {
+        agent = navigator.userAgent;
 
-    userBrowser += " - " + agent
+        if (agent.indexOf("Edge") > -1) {
+            userBrowser = "Edge";
+        }
+        else if (agent.indexOf("Chrome") > -1) {
+            userBrowser = "Chrome";
+        }
+        else if (agent.indexOf("Safari") > -1) {
+            userBrowser = "Safari";
+        }
+        else if (agent.indexOf("Opera") > -1) {
+            userBrowser = "Opera";
+        }
+        else if (agent.indexOf("Firefox") > -1) {
+            userBrowser = "Firefox";
+        }
+        else if (agent.indexOf("MSIE") > -1) {
+            userBrowser = "Internet Explorer";
+        }
+        else if (agent.indexOf("Trident") > -1) {
+            userBrowser = "Internet Explorer";
+        }
+        userBrowser += " - " + agent;
+    }
+    else {
+        userBrowser = "Other";
+    }
+    
     return userBrowser;
 }
