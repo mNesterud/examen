97980b55d07f641b7370dd1cb65bac47e9cbb292
mNesterud <mnesterud@gmail.com>
2017-04-28 14:59:45 +0200

    Flyttat Start till egen view-mapp och skapat egen controller

---
+++ Examen/Controllers/StartController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Examen.Controllers
+{
+    public class StartController : Controller
+    {
+        // GET: Start
+        public ActionResult Start()
+        {
+            return View();
+        }
+    }
+}
